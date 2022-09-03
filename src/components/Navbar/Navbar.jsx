b0f099e77079eb1d7baffc7d8b1f8417a9ad21ca
Biswajit Das <biswajitdas7894@gmail.com>
2022-09-04 02:18:46 +0530

    nav links added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,6 +5,14 @@ const Navbar = () => {
   return (
     <nav className={styles.navContainer}>
       <h1>Biswajit Das</h1>
+      <div>
+        <a href="#">Menu</a>
+        <a href="#">About</a>
+        <a href="#">Projects</a>
+        <a href="#">Blogs</a>
+        <a href="#">Contact US</a>
+        <a href="#">Change Color</a>
+      </div>
     </nav>
   );
 };
