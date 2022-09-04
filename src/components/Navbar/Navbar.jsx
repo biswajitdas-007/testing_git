910250b330e193fd5d39fb79c5c949689ccf3ad7
Biswajit Das <biswajitdas7894@gmail.com>
2022-09-04 15:08:08 +0530

    Navbar update

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,9 @@
 import React from "react";
-<<<<<<< HEAD
 import styles from "./Navbar.module.css";
 
 const Navbar = () => {
   return (
     <nav className={styles.navContainer}>
-=======
-
-const Navbar = () => {
-  return (
-    <nav>
->>>>>>> 0e81041 (Component folder added and navbar file is added)
       <h1>Biswajit Das</h1>
     </nav>
   );
